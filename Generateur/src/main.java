f76a8c672638e12d43582356e79bad53112cf998
GuillaumeHaben <pyxouze@gmail.com>
2015-05-06 16:04:18 +0200

    Amélioration des tests pour le chargement de .bi + boucle do pour le menu | main

--- Generateur/src/main.java
+++ Generateur/src/main.java
@@ -7,55 +7,62 @@ public class main {
 	public static void main(String[] args) {
 
 		/* ---------- Création de la vidéothèque ---------- */
-		
-		Videotheque ma_videotheque = new Videotheque();
-		
-		/* --------------------- Menu --------------------- */
-		
-		Scanner scan1 = new Scanner(System.in);
-		
-		System.out.println("Choisissez une opération a effectuer:"); 
-        System.out.println("[1] Créer une Bibliothèque"); 
-        System.out.println("[2] Charger une Bibliothèque déjà existante (format .bi)"); 
-        System.out.println("[3] Quitter"); 
-        System.out.print("Selection: "); 
-        
-        int choix = scan1.nextInt();     
-       
-       switch (choix){
-       		case 1:
-       			ma_videotheque.creer("films.txt");
-       			break;
-             
-       		case 2:
-       			Scanner scan2 = new Scanner(System.in);
-       	        System.out.print("Nom du fichier .bi : "); 
-       	        String fichier = scan2.nextLine();
-       	        
-       	        String extension = fichier.substring(0, fichier.lastIndexOf('.'));
-       	        File filename = new File(fichier);
-       	        
-       	        if (extension.compareTo("bi") != 0) {
-       	        	System.out.println("Le fichier renseigné n'est pas valide");
-       	        }
-       	        else if (!filename.exists()) {
-       	        	System.out.println("Le fichier renseigné n'existe pas");
-       	        }
-       	        else {
-    				try {
-    					ma_videotheque.charger(fichier);
-    				} catch (ClassNotFoundException | IOException e) {
-    					e.printStackTrace();
-    				}
-       	        }
 
-       			break;
-             
-       		case 3:System.out.println("Sorti");
-                System.exit(0);
-                        
-       		default:System.out.println("Entrez une sélection valide");
-           
-       };
+		Videotheque ma_videotheque = new Videotheque();
+
+		/* --------------------- Menu --------------------- */
+
+		Scanner scan1 = new Scanner(System.in);
+
+		System.out.println("Choisissez une opération a effectuer:");
+		System.out.println("[1] Créer une Bibliothèque");
+		System.out.println("[2] Charger une Bibliothèque déjà existante (format .bi)");
+		System.out.println("[3] Quitter");
+		System.out.print("Selection: ");
+
+		int choix = scan1.nextInt();
+
+		boolean fin = false;
+		do {
+			switch (choix) {
+			case 1:
+				ma_videotheque.creer("films.txt");
+				break;
+
+			case 2:
+				// L'utilisateur renseigne le nom du fichier .bi existant
+				Scanner scan2 = new Scanner(System.in);
+				System.out.print("Nom du fichier .bi : ");
+				String fichier = scan2.nextLine();
+
+				// On associe l'entrée de l'utilisateur à un nouvel objet de
+				// type File
+				File filename = new File(fichier);
+
+				// On vérifie qu'il a la bonne extension
+				if (!fichier.endsWith(".bi")) {
+					System.out.println("Le fichier renseigné n'est pas valide");
+				}
+				// On vérifie que le fichier existe réellement
+				else if (!filename.exists()) {
+					System.out.println("Le fichier renseigné n'existe pas");
+				} else {
+					/*
+					 * try { ma_videotheque.charger(fichier); } catch
+					 * (ClassNotFoundException | IOException e) {
+					 * e.printStackTrace(); }
+					 */
+					fin = true;
+				}
+				break;
+
+			case 3:
+				System.out.println("Sorti");
+				System.exit(0);
+
+			default:
+				System.out.println("Entrez une sélection valide");
+			};
+		} while (!fin);
 	}
 }
