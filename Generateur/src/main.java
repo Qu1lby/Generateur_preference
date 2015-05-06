67598e481bad5899bad57881bf7860ecb0e45127
GuillaumeHaben <pyxouze@gmail.com>
2015-05-06 14:12:31 +0200

    Amélioration du commit précédent (menu) | main

--- Generateur/src/main.java
+++ Generateur/src/main.java
@@ -21,32 +21,31 @@ public class main {
 		Ressource HP = new Ressource(titre, annee, synopsis, acteurs, genres, type, realisateur, note, vu);
 		System.out.println(HP.toString());
 		
-		Scanner sc = new Scanner(System.in);
+		/* ---------- Menu ---------- */
 		
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Choisissez une opération a effectuer:"); 
         System.out.println("[1] Créer une Bibliothèque (format .txt)"); 
-        System.out.println("[2] Charger une Bibliothèque déjà existante (format .bi"); 
+        System.out.println("[2] Charger une Bibliothèque déjà existante (format .bi)"); 
         System.out.println("[3] Quitter"); 
         
         System.out.println("Selection: "); 
         int selection = sc.nextInt();     
        
        switch (selection){
+       		case 1:
+       			Videotheque v1 = new Videotheque();
+       			v1.creer("films.txt");
+       			break;
              
-           case 1:
-        	   	Videotheque v1 = new Videotheque();
-        	   	v1.creer("fichier");
-                break;
+       		case 2:
+       			
+       			break;
              
-        
-           case 2:System.out.println("Bilbliothèque chargée");
-                break;
-             
-        
-           case 3:System.out.println("Sorti");
+       		case 3:System.out.println("Sorti");
                 System.exit(0);
                         
-           default:System.out.println("Entrez une sélection valide");
+       		default:System.out.println("Entrez une sélection valide");
            
        };
 		
