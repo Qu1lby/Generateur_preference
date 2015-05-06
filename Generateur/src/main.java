52cd3bbc079a2b494a5960a3ec47eb31161e7ed2
GuillaumeHaben <pyxouze@gmail.com>
2015-05-06 09:50:55 +0200

    Main | Première ébauche d'un menu console

--- Generateur/src/main.java
+++ Generateur/src/main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class main {
 
@@ -19,6 +20,34 @@ public class main {
 		
 		Ressource HP = new Ressource(titre, annee, synopsis, acteurs, genres, type, realisateur, note, vu);
 		System.out.println(HP.toString());
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Choisissez une opération a effectuer:"); 
+        System.out.println("[1] Créer une Bibliothèque (format .txt)"); 
+        System.out.println("[2] Charger une Bibliothèque déjà existante (format .bi"); 
+        System.out.println("[3] Quitter"); 
+        
+        System.out.println("Selection: "); 
+        int selection = sc.nextInt();     
+       
+       switch (selection){
+             
+           case 1:System.out.println("Bilbliothèque créée");
+                break;
+             
+        
+           case 2:System.out.println("Bilbliothèque chargée");
+                break;
+             
+        
+           case 3:System.out.println("Sorti");
+                System.exit(0);
+                        
+           default:System.out.println("Entrez une sélection valide");
+           
+       };
+		
 	}
 	
 }
