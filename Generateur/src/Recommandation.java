b4a926ad300ffcbbbccd31606b3fa9946ef8ff5c
GuillaumeHaben <pyxouze@gmail.com>
2015-05-23 23:28:10 +0200

    Ajout des méthodes init et afficher | Recommandation

--- Generateur/src/Recommandation.java
+++ Generateur/src/Recommandation.java
@@ -1,17 +1,19 @@
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
 
-public abstract class Recommandation {
+public class Recommandation {
 	/**
 	 * Permet de créer une liste de recommandation de films à partir des films de la vidéothèque 
 	 * @param source : Liste de Ressources déjà visionnées
 	 * @return code retour
 	 */
+	ArrayList<Ressource> liste_des_similaires = new ArrayList<Ressource>();
 	
-	public static void init(Videotheque v) {
+	public Recommandation() {
+		liste_des_similaires = null;
+	}
+	
+	public void init(Videotheque v) {
 		ArrayList<Ressource> liste_films_vus = v.list_films_sup_moy();
-		ArrayList<Ressource> liste_des_similaires = new ArrayList<Ressource>();
 		
 		for(int i=0; i<liste_films_vus.size(); i++) {
             ArrayList<Association> liste = liste_films_vus.get(i).getSimilaire();
@@ -24,5 +26,21 @@ public abstract class Recommandation {
             }
     	}
 	}
+	
+	public void afficher() {
+		for(int i=0; i<liste_des_similaires.size(); i++) {
+			liste_des_similaires.get(i).toString();
+		}
+	}
+
+	/**
+	 * @return the liste_des_similaires
+	 */
+	public ArrayList<Ressource> getListe_des_similaires() {
+		return liste_des_similaires;
+	}
+	
+	
+	
 }
 
